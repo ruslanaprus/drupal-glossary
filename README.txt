11de3902f91d82063f385321f2eaf0ab6c5e798f
ruslana <ruslana.prus@ukr.net>
2026-04-03 19:12:10 +0300

    use use plugin to filter content

---
+++ README.txt
@@ -0,0 +1,14 @@
+ABOUT GLOSSARY TOOLTIP MODULE
+------------------------------------
+
+GLOSSARY TOOLTIP FILTER
+----------------
+
+The Glossary Tooltip Filter automatically finds words from the site’s glossary and adds a tooltip with their description. This filter makes your content easier to read by showing quick explanations for important words. If a word is listed in the site’s glossary, it will be highlighted, and readers can see its meaning when they move their mouse over it.
+
+How to turn it on:
+Go to Configuration → Content authoring → Text formats and editors. Edit the text format you use (for example, Basic HTML). In the list of filters, check Glossary Tooltip Filter. Save your changes.
+
+For plugin to work:
+Glossary terms must be added in the “glossary” vocabulary with a description.
+Place this filter after the basic HTML cleanup filter.
---
+++ src/Plugin/Filter/GlossaryFilter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\glossary_tooltip\Plugin\Filter;
+
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\filter\FilterProcessResult;
+use Drupal\glossary_tooltip\Service\GlossaryProcessor;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @Filter(
+ *   id = "glossary_filter",
+ *   title = @Translation("Glossary Tooltip Filter"),
+ *   description = @Translation("Adds tooltips for glossary terms."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE
+ * )
+ */
+class GlossaryFilter extends FilterBase implements ContainerFactoryPluginInterface {
+
+  protected GlossaryProcessor $processor;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, GlossaryProcessor $processor) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->processor = $processor;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('glossary_tooltip.processor')
+    );
+  }
+
+  public function process($text, $langcode) {
+    $processed = $this->processor->process($text);
+    return new FilterProcessResult($processed);
+  }
+}
